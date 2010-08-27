a70444a6894515aa83c55f893ba6ec5a4ea0e8ed
tekimaki <will@tekimaki.com>
2010-08-27 15:28:22 -0400

    isolate installer requirements to run only at install time

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,15 +1,22 @@
 <?php
-global $gBitInstaller;
-$gBitInstaller->registerPackageInfo( FCKEDITOR_PKG_NAME, array(
+
+global $gBitSystem;
+
+$gBitSystem->registerPackageInfo( FCKEDITOR_PKG_NAME, array(
 	'description' => "FCKEditor is a 'What You See Is What You Get' textarea HTML editor. It works with IE 5.5+, Firefox 1.0+, Mozilla 1.3+, and Netscape 7+. ",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
 	'important' => 'When using this WYSIWYG editor, we recommend that you either use HTML as the only content format or use TikiWiki with the option <kbd>allow HTML</kbd> enabled (formats are set in Administration --> Liberty --> Liberty plugins). It <em>can not</em> be used with competing editor TinyMCE active, and it is largely incompatible with the package Quicktags.',
 	'version' => '2.6+',
 ) );
 
+// Install process
+global $gBitInstaller;
+if( is_object( $gBitInstaller ) ){
+
 $gBitInstaller->registerPreferences( FCKEDITOR_PKG_NAME, array(
 	array(FCKEDITOR_PKG_NAME,'fckeditor_debug','n'),
 	array(FCKEDITOR_PKG_NAME,'fckeditor_toolbars', 'Bitweaver'),
 ) );
 
-?>
+}
+
